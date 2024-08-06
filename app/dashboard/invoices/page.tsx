7757930ff71e27942abdf9da6f79f6b06b0bc08f
Dubea <dubea001@gmail.com>
2024-08-06 13:23:38 +0100

    invoice/page.tsx file adjusted

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,15 +8,15 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
-    searchParam,
+    searchParams,
 }: {
-    searchParam?: {
+    searchParams?: {
         query?: string;
         page?: string;
     };
 }) {
-    const query = searchParam?.query || '';
-    const currentPage = Number(searchParam?.page) || 1;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
     const totalPages = await fetchInvoicesPages(query);
     return (
         <div className='w-full'>
